3760e80276f323d83804dee2d977613b7c8a0a97
Youssef-AJDIJ <91636601+Youssef-AJDIJ@users.noreply.github.com>
2023-02-22 15:51:43 +0100

    Add files via upload

---
+++ Mi Portfolio Con BootStrap/js/jquery.js
@@ -0,0 +1,40 @@
+$(function () {
+    $('#Mutoh').click(function () {
+        $('#Load-Video').html('<video src="videos/video mutoh xperjet 1641.mp4"width="100%" controls autoplay></video>')
+    })
+})
+$(function () {
+    $('#CNC').click(function () {
+        $('#Load-Video').html('<video src="videos/video machine CNC Servo motors.mp4"width="100%" controls muted autoplay></video>')
+    })
+})
+$(function () {
+    $('#Summa').click(function () {
+        $('#Load-Video').html('<video src="videos/video machine plotter summa.mp4"width="100%" controls muted autoplay></video>')
+    })
+})
+$(function () {
+    $('#Bodor').click(function () {
+        $('#Load-Video').html('<video src="videos/Laser con camara.mp4"width="100%" controls muted autoplay></video>')
+    })
+})
+$(function () {
+    $('#Erick').click(function () {
+        $('#Load-Video').html('<video src="videos/video machine dtf Erick.mp4"width="100%" controls muted autoplay></video>')
+    })
+})
+$(function () {
+    $('#Plazma').click(function () {
+        $('#Load-Video').html('<video src="videos/video machine plasma.mp4"width="100%" controls muted autoplay></video>')
+    })
+})
+$(function () {
+    $('#Smt-jet').click(function () {
+        $('#Load-Video').html('<video src="videos/Installation machine 320 SMT JET with four heads -starfire- impression 3 Pass.mp4"width="100%"  controls muted autoplay></video>')
+    })
+})
+$(function () {
+    $('#Farmacia').click(function () {
+        $('#Load-Video').html('<video src="videos/decrencent Farmacia .mp4"width="100%" controls muted autoplay></video>')
+    })
+})
